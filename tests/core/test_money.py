d852c3e78cf518504443720a45bbc103f1b0f6bf
Robbertjan <robbertjan-1994@hotmail.com>
2021-10-28 21:23:09 +0200

    TEST: tests for arithmatic dunder methods money

--- tests/core/test_money.py
+++ tests/core/test_money.py
@@ -3,11 +3,9 @@ from perfinpy.core.money import Currency, Money
 
 ##### Currency ################################################################
 
-def test_currency_valid():
+def test_currency_init():
     currency = Currency('EUR')
     assert currency.acronym == 'EUR'
-
-def test_currency_invalid():
     with pytest.raises(ValueError):
         Currency('EURO')
 
@@ -19,12 +17,12 @@ def test_currency_equality():
 
 ##### Money ###################################################################
 
-def test_money_initialize():
+def test_money_init():
     money_1 = Money(1)
     assert money_1.value == 1
-    usd = Currency('USD')
-    usd_1 = Money(1, usd)
-    assert usd_1.currency == usd
+    eur = Currency('EUR')
+    eur_1 = Money(1, eur)
+    assert eur_1.currency == eur
 
 def test_money_equality():
     eur_1 = Money(1, Currency('EUR'))
@@ -39,3 +37,52 @@ def test_money_compatible():
     assert eur_1.compatible(eur_1)
     usd_1 = Money(1, Currency('USD'))
     assert not eur_1.compatible(usd_1)
+
+def test_money_add():
+    eur = Currency('EUR')
+    eur_1 = Money(1, eur)
+    eur_5 = Money(5, eur)
+    assert eur_1 + eur_5 == Money(6, eur)
+    assert eur_1 + 5 == Money(6, eur)
+    assert 5 + eur_1 == Money(6, eur)
+    usd = Currency('USD')
+    usd_1 = Money(1, usd)
+    with pytest.raises(TypeError):
+        eur_1 + usd_1
+
+def test_money_subtract():
+    eur = Currency('EUR')
+    eur_1 = Money(1, eur)
+    eur_5 = Money(5, eur)
+    assert eur_5 - eur_1 == Money(4, eur)
+    assert eur_5 - 1 == Money(4, eur)
+    assert 9 - eur_5 == Money(4, eur)
+    assert eur_1 - eur_5 == Money(-4, eur)
+    usd = Currency('USD')
+    usd_1 = Money(1, usd)
+    with pytest.raises(TypeError):
+        eur_1 - usd_1
+
+def test_money_multiply():
+    eur = Currency('EUR')
+    eur_2 = Money(2, eur)
+    eur_5 = Money(5, eur)
+    assert eur_2 * eur_5 == Money(10, Currency('EUR'))
+    assert eur_2 * 5 == Money(10, Currency('EUR'))
+    assert 2 * eur_5 == Money(10, Currency('EUR'))
+    usd = Currency('USD')
+    usd_1 = Money(1, usd)
+    with pytest.raises(TypeError):
+        eur_2 * usd_1
+
+def test_money_divide():
+    eur = Currency('EUR')
+    eur_2 = Money(2, eur)
+    eur_4 = Money(4, eur)
+    assert eur_4 / eur_2 == Money(2, Currency('EUR'))
+    assert eur_4 / 2 == Money(2, Currency('EUR'))
+    assert 2 / eur_4 == Money(0.5, Currency('EUR'))
+    usd = Currency('USD')
+    usd_1 = Money(1, usd)
+    with pytest.raises(TypeError):
+        eur_2 / usd_1
